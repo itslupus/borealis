5a693349b9fadf72255ebdc22b10a9d217f4c566
Lucas <contact@itslucas.win>
2020-06-04 21:27:59 -0500

    Removed PHP style views

--- view/alpha/index.php
+++
@@ -1,6 +0,0 @@
-<p>welcome screen, login</p>
-<form action = '/logic/Login.php' method = POST autocomplete = off>
-    <input name = 'id' type = 'text' placeholder = 'student number'><br>
-    <input name = 'password' type = 'password' placeholder = 'password'><br>
-    <input type = 'submit' value = 'login'><br>
-</form>
